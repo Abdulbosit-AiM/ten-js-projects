1b0ac123d336625e77cc7827da619762cd582c6d
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2022-01-26 16:56:50 +0500

    add html layout + styles

---
+++ 2.quiz-app/script.js
@@ -0,0 +1,50 @@
+const quizData = [
+    {
+        question: 'How old is Abdulbosit?',
+        a: '21',
+        b: '17',
+        c: '26',
+        d: '100',
+        correct: 'a'
+    },
+    {
+        question: 'What was the most used programming language in 2021?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'C++',
+        correct: 'b'
+    },
+    {
+        question: 'How old is Abdulbosit',
+        a: '21',
+        b: '17',
+        c: '26',
+        d: '100',
+        correct: 'c'
+    },
+    {
+        question: 'Who is the founder of Microsoft?',
+        a: 'Abdulbosit',
+        b: 'Paul Allen',
+        c: 'Bill Gates',
+        d: 'Steve Jobs',
+        correct: 'c'
+    },
+    {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheets',
+        c: 'JavaScript Object Notation',
+        d: 'North Atlantic Treaty Organization',
+        correct: 'a'
+    },
+    {
+        question: 'When was JavaScript launched?',
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'none of the above',
+        correct: 'b'
+    }
+]
