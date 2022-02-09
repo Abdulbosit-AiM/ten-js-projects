4a589b89aa9b35740dc875428e295a3a26c10deb
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2022-02-09 23:57:40 +0500

    + getmovies func

--- 6.movies-app/script.js
+++ 6.movies-app/script.js
@@ -1,2 +1,14 @@
-const APIKEY = '44346d7d3ab51accb2a7fa7cbd937920'
-const API_URL = 'https://api.themoviedb.org/3/movie/550?api_key='
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=44346d7d3ab51accb2a7fa7cbd937920'
+const IMGPATH = ''
+
+
+async function getMovies() {
+    const resp = await fetch(API_URL)
+    const respData = await resp.json()
+
+    console.log(respData)
+
+    return respData
+}
+
+getMovies()
